cb62134a69a52255c6bb9079a0f352379e7d8553
umairaltaf982 <umairaltaf982@gmail.com>
2024-07-09 02:26:07 +0500

    object topic completed

--- js files/objects.js
+++ js files/objects.js
@@ -33,3 +33,59 @@ document.getElementById("demo").innerHTML = student3.name + " is of age " + stud
 //A JAvascript object is basically a collection of unordered properties
 //Values associated with a Javascript Object are called its properties
 //Properties can usually be added, updated, deleted, excluding read-only properties
+//lets have an example
+var student1 = {
+    name1: "Umair",                          //Property
+    age: 21,                                 //property
+    dept: "CS",                              //property
+};
+//we can override any of the property
+student1.name1 = "Ammar";                   //overriding property
+document.getElementById("demo").innerHTML = student1.name1 + " is of age " + student1.age + " is in department " + student1.dept;
+
+
+
+//Javascript Methods
+// def: A javascript method is an action that can be performed on the objects
+// A javascript method is a property of an object that is a function
+// lets have an example
+let data = {
+    name: "Farhan",
+    age: 45,
+}
+function sayHi() {
+    alert("Hello " + data.name);
+}
+data.message = sayHi;             //adding a new property to the object named data
+document.getElementById("demo").innerHTML = "Hi! " + data.name + " of the age " + data.age;
+document.getElementById("click").onclick = data.message;
+
+
+
+//Javascript object Accessors
+// def: An accessor is a function that is called when you try to access a property
+
+//getter is used to get the value of the properties of the object 
+//Example of getter
+var car = {
+    name: "Mustang",
+    model: "1998",
+    feul_type: "Diesel",
+    get feul() {
+        return this.feul_type;
+    },
+}
+document.getElementById("demo").innerHTML = car.name + " is a " + car.model + " and it is a " + car.feul + " car";
+
+
+//setter is used to set the value of the properties of the object
+//Example of setter
+var cat = {
+    name: "Tom",
+    cat_age: 5,
+    set age(age) {
+        this.cat_age = age;
+    }
+}
+cat.age = 6;
+document.getElementById("demo").innerHTML = cat.name + " is " + cat.cat_age + " years";
